12460d6d822fa526cd071a678e6e9c8ebf880368
parallelWorld <654269765@qq.com>
2016-03-30 13:13:54 +0800

    增加LaunchImage和RootViewController

---
+++ Example/HYCoreFramework/RootViewController/SLRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYRootViewController.h
+//  HYCoreFramework
+//
+//  Created by 58 on 3/30/16.
+//  Copyright © 2016 fangyuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLRootViewController : UIViewController
+
+@end
--- Pod/Classes/Categorys/UIView+HYEmptyDataSet.m
+++ Pod/Classes/Categorys/UIView+HYEmptyDataSet.m
@@ -33,7 +33,7 @@
 
 - (void)reloadEmptyDataSet
 {
-    if ([self hy_canDisplay] && [self hy_itemsCount] == 0) {
+    if ([self hy_canDisplay]) {
         
         [self hy_willAppear];
         
@@ -77,11 +77,6 @@
                                                                              attribute:NSLayoutAttributeCenterY
                                                                             multiplier:1.0
                                                                               constant:0.0];
-        
-        if (self.hy_emptyDataSetSource && [self.hy_emptyDataSetSource respondsToSelector:@selector(hy_verticalOffsetForEmptyDataSet:)]) {
-            centerYConstraint.constant = [self.hy_emptyDataSetSource hy_verticalOffsetForEmptyDataSet:self];
-        }
-        
         [containerView addConstraint:centerXConstraint];
         [containerView addConstraint:centerYConstraint];
     
@@ -113,15 +108,6 @@
         [self hy_invalidate];
     }
     objc_setAssociatedObject(self, @selector(hy_emptyDataSetSource), hy_emptyDataSetSource, OBJC_ASSOCIATION_ASSIGN);
-    
-    // We add method sizzling for injecting -dzn_reloadData implementation to the native -reloadData implementation
-    [self swizzleIfPossible:@selector(reloadData)];
-    
-    // Exclusively for UITableView, we also inject -dzn_reloadData to -endUpdates
-    if ([self isKindOfClass:[UITableView class]]) {
-        [self swizzleIfPossible:@selector(endUpdates)];
-    }
-
 }
 
 - (void)setHy_emptyDataSetDelegate:(id<HYEmptyDataSetDelegate>)hy_emptyDataSetDelegate
@@ -183,152 +169,11 @@
     return view;
 }
 
-- (NSInteger)hy_itemsCount
-{
-    NSInteger items = 0;
-    
-    // UIScollView doesn't respond to 'dataSource' so let's exit
-    if (![self respondsToSelector:@selector(dataSource)]) {
-        return items;
-    }
-    
-    // UITableView support
-    if ([self isKindOfClass:[UITableView class]]) {
-        
-        UITableView *tableView = (UITableView *)self;
-        id <UITableViewDataSource> dataSource = tableView.dataSource;
-        
-        NSInteger sections = 1;
-        
-        if (dataSource && [dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
-            sections = [dataSource numberOfSectionsInTableView:tableView];
-        }
-        
-        if (dataSource && [dataSource respondsToSelector:@selector(tableView:numberOfRowsInSection:)]) {
-            for (NSInteger section = 0; section < sections; section++) {
-                items += [dataSource tableView:tableView numberOfRowsInSection:section];
-            }
-        }
-    }
-    // UICollectionView support
-    else if ([self isKindOfClass:[UICollectionView class]]) {
-        
-        UICollectionView *collectionView = (UICollectionView *)self;
-        id <UICollectionViewDataSource> dataSource = collectionView.dataSource;
-        
-        NSInteger sections = 1;
-        
-        if (dataSource && [dataSource respondsToSelector:@selector(numberOfSectionsInCollectionView:)]) {
-            sections = [dataSource numberOfSectionsInCollectionView:collectionView];
-        }
-        
-        if (dataSource && [dataSource respondsToSelector:@selector(collectionView:numberOfItemsInSection:)]) {
-            for (NSInteger section = 0; section < sections; section++) {
-                items += [dataSource collectionView:collectionView numberOfItemsInSection:section];
-            }
-        }
-    }
-    
-    return items;
-}
-
 #pragma mark - Setters
 
 - (void)setEmptyDataSetContainerView:(UIView *)emptyDataSetContainerView
 {
     objc_setAssociatedObject(self, @selector(emptyDataSetContainerView), emptyDataSetContainerView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-#pragma mark - Method Swizzling
-
-static NSMutableDictionary *_impLookupTable;
-static NSString *const HYSwizzleInfoPointerKey = @"pointer";
-static NSString *const HYSwizzleInfoOwnerKey = @"owner";
-static NSString *const HYSwizzleInfoSelectorKey = @"selector";
-
-// Based on Bryce Buchanan's swizzling technique http://blog.newrelic.com/2014/04/16/right-way-to-swizzle/
-// And Juzzin's ideas https://github.com/juzzin/JUSEmptyViewController
-
-void hy_original_implementation(id self, SEL _cmd)
-{
-    // Fetch original implementation from lookup table
-    NSString *key = hy_implementationKey(self, _cmd);
-    
-    NSDictionary *swizzleInfo = [_impLookupTable objectForKey:key];
-    NSValue *impValue = [swizzleInfo valueForKey:HYSwizzleInfoPointerKey];
-    
-    IMP impPointer = [impValue pointerValue];
-    
-    // We then inject the additional implementation for reloading the empty dataset
-    // Doing it before calling the original implementation does update the 'isEmptyDataSetVisible' flag on time.
-    [self reloadEmptyDataSet];
-    
-    // If found, call original implementation
-    if (impPointer) {
-        ((void(*)(id,SEL))impPointer)(self,_cmd);
-    }
-}
-
-NSString *hy_implementationKey(id target, SEL selector)
-{
-    if (!target || !selector) {
-        return nil;
-    }
-    
-    Class baseClass;
-    if ([target isKindOfClass:[UITableView class]]) baseClass = [UITableView class];
-    else if ([target isKindOfClass:[UICollectionView class]]) baseClass = [UICollectionView class];
-    else if ([target isKindOfClass:[UIScrollView class]]) baseClass = [UIScrollView class];
-    else return nil;
-    
-    NSString *className = NSStringFromClass([baseClass class]);
-    
-    NSString *selectorName = NSStringFromSelector(selector);
-    return [NSString stringWithFormat:@"%@_%@",className,selectorName];
-}
-
-- (void)swizzleIfPossible:(SEL)selector
-{
-    // Check if the target responds to selector
-    if (![self respondsToSelector:selector]) {
-        return;
-    }
-    
-    // Create the lookup table
-    if (!_impLookupTable) {
-        _impLookupTable = [[NSMutableDictionary alloc] initWithCapacity:2];
-    }
-    
-    // We make sure that setImplementation is called once per class kind, UITableView or UICollectionView.
-    for (NSDictionary *info in [_impLookupTable allValues]) {
-        Class class = [info objectForKey:HYSwizzleInfoOwnerKey];
-        NSString *selectorName = [info objectForKey:HYSwizzleInfoSelectorKey];
-        
-        if ([selectorName isEqualToString:NSStringFromSelector(selector)]) {
-            if ([self isKindOfClass:class]) {
-                return;
-            }
-        }
-    }
-    
-    NSString *key = hy_implementationKey(self, selector);
-    NSValue *impValue = [[_impLookupTable objectForKey:key] valueForKey:HYSwizzleInfoPointerKey];
-    
-    // If the implementation for this class already exist, skip!!
-    if (impValue || !key) {
-        return;
-    }
-    
-    // Swizzle by injecting additional implementation
-    Method method = class_getInstanceMethod([self class], selector);
-    IMP dzn_newImplementation = method_setImplementation(method, (IMP)hy_original_implementation);
-    
-    // Store the new implementation in the lookup table
-    NSDictionary *swizzledInfo = @{HYSwizzleInfoOwnerKey: [self class],
-                                   HYSwizzleInfoSelectorKey: NSStringFromSelector(selector),
-                                   HYSwizzleInfoPointerKey: [NSValue valueWithPointer:dzn_newImplementation]};
-    
-    [_impLookupTable setObject:swizzledInfo forKey:key];
 }
 
 @end
